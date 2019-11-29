4fb1841928efd02e91d059ef33437a9585ce5a4a
Galy <emilie.lepine@epitech.eu>
2019-11-29 14:45:21 +0100

    added background and modified title

--- client/screens/LandingScreen.js
+++ client/screens/LandingScreen.js
@@ -5,6 +5,7 @@ import {
     AsyncStorage,
     StyleSheet,
     Dimensions,
+    ImageBackground
 } from 'react-native';
 import { Container, Content, Button, Text, View, Toast, Spinner } from 'native-base';
 import api from "../services/api";
@@ -48,26 +49,28 @@ export default class LandignScreen extends Component {
     render() {
         return (
             <Container style={styles.homescreen}>
-                {this.state.isAutoLogin && (
-                    <View>
-                        <Spinner/>
-                        <Text style={{color: 'white', textAlign: 'center'}}>Logging in...</Text>
-                    </View>
-                )}
-                {!this.state.isAutoLogin && (
-                    <View style={styles.homescreen}>
-                        <Text style={styles.logoText}>OsefChat</Text>
-                        <Button style={styles.button1}
-                            onPress={() => {
-                                this.props.navigation.navigate('Login')
-                            }}
+                <ImageBackground source={require('../assets/images/staline.jpg')} style={styles.images}>
+                    {this.state.isAutoLogin && (
+                        <View>
+                            <Spinner/>
+                            <Text style={{color: 'white', textAlign: 'center'}}>Logging in...</Text>
+                        </View>
+                    )}
+                    {!this.state.isAutoLogin && (
+                        <View style={styles.homescreen}>
+                            <Text style={styles.logoText}>Снапчате</Text>
+                            <Button style={styles.button1}
+                                    onPress={() => {
+                                        this.props.navigation.navigate('Login')
+                                    }}
                             ><Text>Login</Text></Button>
-                        <Button style={styles.button2}
-                            onPress={() => {
-                                this.props.navigation.navigate('Register')
-                            }}
+                            <Button style={styles.button2}
+                                    onPress={() => {
+                                        this.props.navigation.navigate('Register')
+                                    }}
                             ><Text>Register</Text></Button>
-                    </View>)}
+                        </View>)}
+                </ImageBackground>
             </Container>
         );
     }
@@ -75,35 +78,44 @@ export default class LandignScreen extends Component {
 
 var width = Dimensions.get('window').width;
 const styles = StyleSheet.create({
-  homescreen:{
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: '#441444',
-  },
-  logoText:{
-    color: '#ffffff',
-    fontSize: 31,
-    paddingBottom: 50,
-  },
-  button1:{
-      backgroundColor: "#9d2e9d",
-      width: width,
-      justifyContent: "center",
-      alignItems: "center",
-      height: 50,
-      marginTop: 20,
-      position: 'absolute',
-      bottom:0,
-  },
-  button2:{
-      backgroundColor: "#9230af",
-      width: width,
-      justifyContent: "center",
-      alignItems: "center",
-      height: 50,
-      // marginTop: 20,
-      position: 'absolute',
-      bottom:50,
-  }
+    homescreen:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        // backgroundColor: '#441444',
+    },
+    logoText:{
+        color: '#ffffff',
+        fontSize: 31,
+        paddingBottom: 50,
+    },
+    button1:{
+        backgroundColor: "#e86431",
+        width: width,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        marginTop: 20,
+        position: 'absolute',
+        bottom:0,
+    },
+    button2:{
+        backgroundColor: "#77331e",
+        width: width,
+        justifyContent: "center",
+        alignItems: "center",
+        height: 50,
+        // marginTop: 20,
+        position: 'absolute',
+        bottom:50,
+    },
+    images:{
+        backgroundColor: '#ccc',
+        flex: 1,
+        position: 'absolute',
+        resizeMode : 'center',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+    }
 });
